e2bf33b86404b0889eaf8f88dab2e5b32780c74e
Mandar Gokhale <manny.gokhale@msg.com>
2022-01-31 05:25:06 -0500

    Add todo to remove hard coded base url

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,7 @@ class InvitationMailer < ApplicationMailer
   def invite_user(inviter_email, invitee_email, invitation_code)
     @inviter_email = inviter_email
     @invitee_email = invitee_email
+    # TODO: Use env var for base url
     @link = "http://localhost:4001/sign_up?token=#{invitation_code}"
     mail(to: @invitee_email, subject: 'Invitation to join Awesome App' )
   end
